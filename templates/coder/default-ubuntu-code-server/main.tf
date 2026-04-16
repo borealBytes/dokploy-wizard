541f1548c1d4fffb229dcd9f6fd6ebdffac11bef
borealBytes <claytoneyoung+github@gmail.com>
2026-04-15 22:56:33 -0400

    Add default Ubuntu VS Code template for Coder

---
+++ templates/coder/default-ubuntu-code-server/main.tf
@@ -0,0 +1,89 @@
+terraform {
+  required_providers {
+    coder = {
+      source = "coder/coder"
+    }
+    docker = {
+      source = "kreuzwerker/docker"
+    }
+  }
+}
+
+provider "coder" {}
+
+variable "docker_socket" {
+  type        = string
+  description = "Optional docker socket URI for the Docker provider."
+  default     = ""
+}
+
+provider "docker" {
+  host = var.docker_socket != "" ? var.docker_socket : null
+}
+
+locals {
+  username = data.coder_workspace_owner.me.name
+}
+
+data "coder_provisioner" "me" {}
+data "coder_workspace" "me" {}
+data "coder_workspace_owner" "me" {}
+
+resource "coder_agent" "main" {
+  arch = data.coder_provisioner.me.arch
+  os   = "linux"
+  dir  = "/home/coder"
+
+  startup_script = <<-EOT
+    set -e
+    if command -v sudo >/dev/null 2>&1; then
+      sudo sh -lc 'apt-get update && apt-get install -y curl git ca-certificates wget'
+    else
+      apt-get update && apt-get install -y curl git ca-certificates wget
+    fi
+  EOT
+}
+
+module "code-server" {
+  count    = data.coder_workspace.me.start_count
+  source   = "registry.coder.com/coder/code-server/coder"
+  version  = "~> 1.0"
+  agent_id = coder_agent.main.id
+  folder   = "/home/coder"
+  order    = 1
+}
+
+resource "docker_volume" "home_volume" {
+  name = "coder-${data.coder_workspace.me.id}-home"
+  lifecycle {
+    ignore_changes = all
+  }
+}
+
+resource "docker_container" "workspace" {
+  count    = data.coder_workspace.me.start_count
+  image    = "codercom/enterprise-base:ubuntu"
+  name     = "coder-${data.coder_workspace_owner.me.name}-${lower(data.coder_workspace.me.name)}"
+  hostname = data.coder_workspace.me.name
+
+  entrypoint = [
+    "sh",
+    "-c",
+    replace(coder_agent.main.init_script, "/localhost|127\\.0\\.0\\.1/", "host.docker.internal"),
+  ]
+
+  env = [
+    "CODER_AGENT_TOKEN=${coder_agent.main.token}",
+  ]
+
+  host {
+    host = "host.docker.internal"
+    ip   = "host-gateway"
+  }
+
+  volumes {
+    container_path = "/home/coder"
+    volume_name    = docker_volume.home_volume.name
+    read_only      = false
+  }
+}
